0a05b2d643e3938fbf271fbbce3d823b49e8df4b
mimasofia <virginiadonaire@hotmail.com>
2018-01-26 11:27:59 -0500

    Add get all and get one in curl requests
    Added two curl requests one for getting the whole list and one for
    getting a specific list item with the id

---
+++ scripts/breed/get_breed.sh
@@ -0,0 +1,4 @@
+curl "http://localhost:4741/breed_lists/${ID}" \
+  --include \
+  --request GET \
+  --header "Authorization: Token token=${TOKEN}"
---
+++ scripts/breed/get_breeds.sh
@@ -0,0 +1,4 @@
+curl "http://localhost:4741/breed_lists" \
+  --include \
+  --request GET \
+  --header "Authorization: Token token=${TOKEN}"
